c1fe92996d8fc040409a67806af268e3fda54198
ygsama <ygsama@qq.com>
2017-05-30 17:06:43 +0800

    Lucene 创建文档及其索引，查询

--- lucene01/src/g/lucene/IndexManagerTest.java
+++ lucene01/src/g/lucene/IndexManagerTest.java
@@ -8,23 +8,27 @@ import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.LongField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.junit.Test;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 /**
- * 使用lucene创建索引和文档
  * @author G
- *
  */
 public class IndexManagerTest {
 
-
+	/**
+	 * 使用lucene创建索引和文档
+	 * @throws Exception
+	 */
 	@Test
 	public void testIndexCreate() throws Exception{
 		
@@ -41,9 +45,29 @@ public class IndexManagerTest {
 			Long fSize = FileUtils.sizeOf(file);
 			
 			// 创建多个域：new TextField(域名,域值 ,是否存储);
+//			TextField fileName = new TextField("fileName",fName ,Store.YES);
+//			TextField fileContent = new TextField("fileContent",fContent ,Store.YES);
+//			TextField fileSize = new TextField("fileSize",fSize.toString() ,Store.YES);
+
+			/**
+			 *  是否分词：要分词，因为要对文件名建立索引，不是是一个整体，有意义
+			 *  是否索引：要索引，因为要通过索引搜索
+			 *  是否存储：要存储，因为要直接显示
+			 */
 			TextField fileName = new TextField("fileName",fName ,Store.YES);
-			TextField fileContent = new TextField("fileContent",fContent ,Store.YES);
-			TextField fileSize = new TextField("fileSize",fSize.toString() ,Store.YES);
+			/**
+			 *  是否分词：要分词，因为要根据文件内容搜索，分词有意义
+			 *  是否索引：要索引，因为要通过索引搜索
+			 *  是否存储：YES或NO都行，是否要直接显示出来
+			 */
+			TextField fileContent = new TextField("fileContent",fContent ,Store.NO);
+			/**
+			 *  是否分词：要分词，因为搜索文档时可以比较数字大小，lucene内置有数字的分词算法
+			 *  是否索引：要索引，因为要通过文件大小搜索
+			 *  是否存储：要存储，因为要直接显示文档大小
+			 */
+			LongField fileSize = new LongField("fileSize",fSize,Store.YES);
+			
 			
 			// 创建一个文档，将所有域放入文档中,文档放入文档列表中
 			Document doc = new Document(); 
@@ -54,7 +78,9 @@ public class IndexManagerTest {
 		}
 		
 		// 创建分词器,StandardAnalyzer标准分词器，支持英文单词，中文单字，(创建和搜索使用的分词器必须一致)
-		Analyzer analyzer = new StandardAnalyzer();
+//		Analyzer analyzer = new StandardAnalyzer();
+		// 中文分词器
+		Analyzer analyzer = new IKAnalyzer();
 		
 		// 指定索引和文档存储的目录
 		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
@@ -73,6 +99,68 @@ public class IndexManagerTest {
 		indexWriter.commit();
 		
 		indexWriter.close();
+	}
+	/**
+	 * 使用lucene删除索引
+	 * @throws Exception
+	 */
+	@Test
+	public void testIndexDel() throws Exception{
 		
+		// 中文分词器
+		Analyzer analyzer = new IKAnalyzer();
+		
+		// 指定索引和文档存储的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		
+		// 创建 文档写对象的配置对象
+		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_4_10_3, analyzer);
+				
+		// 创建 文档写对象 和 索引
+		IndexWriter indexWriter = new IndexWriter(storeDir, indexWriterConfig);
+		
+		// 删除所有
+//		indexWriter.deleteAll();
+		
+		// 词元new Term(域名, 关键字)
+		indexWriter.deleteDocuments(new Term("fileName", "apache"));
+		
+		indexWriter.commit();
+		indexWriter.close();
+	}
+	
+	/**
+	 * 索引和文档的更新
+	 * 更新就是按照传入的Term进行搜索,如果找到结果那么删除,将更新的内容重新生成一个Document对象
+	 * 如果没有搜索到结果,那么将更新的内容直接添加一个新的Document对象
+	 * @throws Exception
+	 */
+	@Test
+	public void testIndexUpdate() throws Exception{
+		
+		// 中文分词器
+		Analyzer analyzer = new IKAnalyzer();
+		
+		// 指定索引和文档存储的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		
+		// 创建 文档写对象的配置对象
+		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_4_10_3, analyzer);
+				
+		// 创建 文档写对象 和 索引
+		IndexWriter indexWriter = new IndexWriter(storeDir, indexWriterConfig);
+		
+		//根据文件名称进行更新
+		Term term = new Term("fileName", "web");
+		//更新的对象
+		Document doc = new Document();
+		doc.add(new TextField("fileName", "xxxxxx", Store.YES));
+		doc.add(new TextField("fileContext", "think in java xxxxxxx", Store.NO));
+		doc.add(new LongField("fileSize", 100L, Store.YES));
+		
+		indexWriter.updateDocument(term, doc);
+		
+		indexWriter.commit();
+		indexWriter.close();
 	}
 }
--- lucene01/src/g/lucene/IndexSearchTest.java
+++ lucene01/src/g/lucene/IndexSearchTest.java
@@ -1,19 +1,27 @@
 package g.lucene;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 /**
  * 搜索lucene中的文档
@@ -22,11 +30,16 @@ import org.junit.Test;
  */
 public class IndexSearchTest {
 
+	/**
+	 * 使用QueryParser查询
+	 * @throws Exception
+	 */
 	@Test
 	public void testIndexSearch() throws Exception{
 		
 		// 创建分词器(创建和搜索使用的分词器必须一致)
-		Analyzer analyzer = new StandardAnalyzer();
+//		Analyzer analyzer = new StandardAnalyzer();
+		Analyzer analyzer = new IKAnalyzer();
 		
 		// c存放索引和文档的目录
 		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
@@ -38,14 +51,78 @@ public class IndexSearchTest {
 		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 		
 		// 创建查询对象,new QueryParser(默认搜索域, 分词器)，查询语法中没指定域则查找默认搜索域
-		QueryParser queryParser = new QueryParser("fileContent", analyzer);
-		
+		QueryParser queryParser = new QueryParser("fileName", analyzer);
+
 		// 查询语法域名：搜索的关键字
 		Query query = queryParser.parse("fileName:apache");
 		
 		// search(查询语句对象, 最大显示条数);
 		TopDocs topDocs = indexSearcher.search(query, 10);
 		
+		// 打印
+		print(indexReader, topDocs);
+	}
+
+	/**
+	 * 使用TermQuery查询
+	 * 只能从文本中搜索
+	 * @throws Exception
+	 */
+	@Test
+	public void testIndexTermQuery() throws Exception{
+		//创建分词器(创建索引和所有时所用的分词器必须一致)
+		Analyzer analyzer = new IKAnalyzer();
+		//指定索引和文档的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		//索引和文档的读取对象
+		IndexReader indexReader = IndexReader.open(storeDir);
+		//创建索引的搜索对象
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		
+		//创建词元:域名+关键字 
+		Term term = new Term("fileName", "apache");
+		//使用TermQuery查询,根据term对象进行查询
+		TermQuery termQuery = new TermQuery(term);
+		// search(查询语句对象, 最大显示条数);
+		TopDocs topDocs = indexSearcher.search(termQuery, 10);
+		
+		// 打印
+		print(indexReader, topDocs);
+	}
+	
+	/**
+	 * 使用NumericRangeQuery通过数字范围查询
+	 * @throws Exception
+	 */
+	@Test
+	public void testNumericRangeQuery() throws Exception{
+		
+		//创建分词器(创建索引和所有时所用的分词器必须一致)
+		Analyzer analyzer = new IKAnalyzer();
+		//指定索引和文档的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		//索引和文档的读取对象
+		IndexReader indexReader = IndexReader.open(storeDir);
+		//创建索引的搜索对象
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		
+		//查询文件大小,大于100 小于1000的文章
+		//NumericRangeQuery.newLongRange(域名, 最小值, 最大值, 是否包含最小值, 是否包含最大值)
+		Query query = NumericRangeQuery.newLongRange("fileSize", 100L, 1000L, true, true);		
+		
+		//搜索:第一个参数为查询语句对象, 第二个参数:指定显示多少条
+		TopDocs topDocs = indexSearcher.search(query, 5);
+		
+		print(indexReader, topDocs);
+	}
+	
+	/**
+	 * 打印topDocs，关闭indexReader流
+	 * @param indexReader
+	 * @param topDocs
+	 * @throws IOException
+	 */
+	private void print(IndexReader indexReader, TopDocs topDocs) throws IOException {
 		// 打印一共多少条
 		System.out.println("搜索记录--"+topDocs.totalHits+"--条");
 		
@@ -55,14 +132,105 @@ public class IndexSearchTest {
 			// 通过id获得文档
 			int docID = scoreDoc.doc;
 			Document document = indexReader.document(docID);
-			
 			System.out.println("fileName:---->"+document.get("fileName"));
 			System.out.println("fileSize:---->"+document.get("fileSize"));
 			System.out.println("fileContent:->"+document.get("fileContent"));
-			
 			System.out.println("*******************************************");
 		}
-		
 		indexReader.close();
+	}
+	
+	/**
+	 * 通过BooleanQuery查询,根据多个条件组合进行查询
+	 * @throws Exception
+	 */
+	@Test
+	public void testBooleanQuery() throws Exception{
+		//创建分词器(创建索引和所有时所用的分词器必须一致)
+		Analyzer analyzer = new IKAnalyzer();
+		
+		//布尔查询,就是可以根据多个条件组合进行查询
+		//文件名称包含apache的,并且文件大小大于等于100 小于等于1000字节的文章
+		BooleanQuery query = new BooleanQuery();
+		
+		//根据数字范围查询
+		//查询文件大小,大于100 小于1000的文章
+		//NumericRangeQuery.newLongRange(域名 , 最小值, 最大值, 是否包含最小值, 是否包含最大值)
+		Query numericQuery = NumericRangeQuery.newLongRange("fileSize", 100L, 1000L, true, true);
+		
+		//创建词元:就是词,   
+		Term term = new Term("fileName", "apache");
+		//使用TermQuery查询,根据term对象进行查询
+		TermQuery termQuery = new TermQuery(term);
+		
+		//Occur是逻辑条件
+		//must相当于and关键字,是并且的意思
+		//should,相当于or关键字或者的意思
+		//must_not相当于not关键字, 非的意思
+		//注意:单独使用must_not  或者 独自使用must_not没有任何意义
+		query.add(termQuery, Occur.MUST);
+		query.add(numericQuery, Occur.MUST);
+		
+		// 指定索引和文档的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		// 索引和文档的读取对象
+		IndexReader indexReader = IndexReader.open(storeDir);
+		// 创建索引的搜索对象
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		// 搜索:第一个参数为查询语句对象, 第二个参数:指定显示多少条
+		TopDocs topDocs = indexSearcher.search(query, 5);
+
+		print(indexReader, topDocs);
+	}
+	
+	/**
+	 * 通过MatchAllDocsQuery查询所有文档
+	 * @throws Exception
+	 */
+	@Test
+	public void testMathAllQuery() throws Exception{
+		// 创建分词器(创建索引和所有时所用的分词器必须一致)
+		Analyzer analyzer = new IKAnalyzer();
+		
+		// 查询所有文档
+		MatchAllDocsQuery query = new MatchAllDocsQuery();
+		
+		// 指定索引和文档的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		// 索引和文档的读取对象
+		IndexReader indexReader = IndexReader.open(storeDir);
+		// 创建索引的搜索对象
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		// 搜索:第一个参数为查询语句对象, 第二个参数:指定显示多少条
+		TopDocs topDocs = indexSearcher.search(query, 5);
+		
+		print(indexReader, topDocs);
+	}
+	
+	/**
+	 * 通过MultiFieldQueryParser，可以从多个域中查询
+	 * @throws Exception
+	 */
+	@Test
+	public void testMultiFieldQueryParser() throws Exception{
+		// 创建分词器(创建索引和所有时所用的分词器必须一致)
+		Analyzer analyzer = new IKAnalyzer();
+		
+		String [] fields = {"fileName","fileContext"};
+		// 从文件名称和文件内容中查询,只有含有apache的就查出来
+		MultiFieldQueryParser multiQuery = new MultiFieldQueryParser(fields, analyzer);
+		// 输入需要搜索的关键字
+		Query query = multiQuery.parse("apache");
+		
+		// 指定索引和文档的目录
+		Directory storeDir = FSDirectory.open(new File("E:\\dic"));
+		// 索引和文档的读取对象
+		IndexReader indexReader = IndexReader.open(storeDir);
+		// 创建索引的搜索对象
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		// 搜索:第一个参数为查询语句对象, 第二个参数:指定显示多少条
+		TopDocs topDocs = indexSearcher.search(query, 5);
+		
+		print(indexReader, topDocs);
 	}
 }

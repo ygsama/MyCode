c69027b909bffb009d55c5bdbb8da8f3e2c1fc19
ygsama <ygsama@qq.com>
2017-06-08 19:53:35 +0800

    Redis(Jedis)测试

---
+++ jedisTest/src/g/jedis/JedisTest.java
@@ -0,0 +1,36 @@
+package g.jedis;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Jedis单独测试
+ * @author G
+ *
+ */
+public class JedisTest {
+
+	@Test
+	public void testJedis1(){
+		// 创建连接
+		Jedis jedis = new Jedis("ip地址", 6379);
+		
+		jedis.set("key1", "1");
+		System.out.println(jedis.get("key1"));
+		jedis.close();
+	}
+	
+	@Test
+	public void testJedisPool() {
+		// 创建连接池
+		JedisPool jedisPool = new JedisPool("ip地址", 6379);
+		
+		Jedis jedis = jedisPool.getResource();
+		jedis.set("key2", "2");
+		System.out.println(jedis.get("key2"));
+		// 关闭后自动回到连接池供其他人获取使用
+		jedis.close();
+	}
+}
